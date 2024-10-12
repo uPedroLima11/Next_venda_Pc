0c0891c08cd4a567dd251bac25176e85cff15e32
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-10-12 18:39:52 -0300

    ADD Avaliation

--- venda_cliente/src/app/detalhes/[produto_id]/page.tsx
+++ venda_cliente/src/app/detalhes/[produto_id]/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 import { Textarea } from "@/components/ui/textarea"
 import { useClienteStore } from "@/context/cliente"
+import { toast } from "@/hooks/use-toast"
 import { ProdutoI } from "@/utils/types/produtos"
+import { ToastAction } from "@radix-ui/react-toast"
 import { useParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
@@ -25,6 +27,38 @@ export default function Detalhes() {
     getDados()
   }, [])
 
+  async function enviaProposta(data: Inputs) {
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/avaliacoes`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({
+        clienteid: cliente.id,
+        produtoId: Number(params.produto_id),
+        descricao: data.descricao,
+        nota:5
+      })
+    })
+  
+    if (response.status == 201) {
+      toast({
+        variant: "default",
+        title: "Proposta enviada",
+        description: "Sua proposta foi enviada com sucesso",
+      });
+      reset()
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Algo deu errado",
+        description: "Verifique suas credenciais e tente novamente",
+        action: <ToastAction altText="Repetir">Repetir</ToastAction>,
+      });
+    }
+  }
+
   return (
     <section className="my-10">
       <h1 className="ms-48 mt-10 mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl">
@@ -43,17 +77,20 @@ export default function Detalhes() {
             Preço: 
             <span className="text-green-600"> R${Number(produto?.preco).toLocaleString("pt-br", { minimumFractionDigits: 2 })}</span>
           </h5>
-          {!cliente.id ? <>
-          <h5 className="text-[#cba35c]">Você deve se logar para realizar uma proposta</h5>
-          </> :
-          <>
-          <div>
-            <form>
-              <h5 className="text-white font-bold">Email: {cliente.email}</h5>
-              <Textarea className="bg-white mt-5" {...register("descricao")} />
-            </form>
-            </div> 
+          {cliente.id ?
+            <>
+              <h3 className="text-xl font-bold tracking-tight mt-5 text-white">Faça uma avalição do produto!</h3>
+              <form onSubmit={handleSubmit(enviaProposta)}>
+                <input type="text" className="mb-2 mt-4 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value={`${cliente.nome} (${cliente.email})`} disabled readOnly />
+                <textarea id="message" className="mb-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  placeholder="Descreva a sua proposta" 
+                  required
+                  {...register("descricao")}></textarea>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar Proposta</button>
+              </form>
             </>
+            :
+            <h3 className="text-xl font-bold tracking-tight text-orange-700">Faça login para fazer proposta para este veículo</h3>
           }
         </div>
       </div>
