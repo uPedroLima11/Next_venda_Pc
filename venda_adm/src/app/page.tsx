fafb6a96ee58acaf2360bcc7e7f21651afb497c8
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-14 09:45:23 -0300

    Changes in ADM zone

--- venda_adm/src/app/page.tsx
+++ venda_adm/src/app/page.tsx
@@ -5,12 +5,11 @@ import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 import { useClienteStore } from "@/context/cliente";
 
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
 type Inputs = {
   email: string;
   senha: string;
-  continuar: boolean;
 };
 
 export default function Login() {
@@ -20,33 +19,22 @@ export default function Login() {
   const router = useRouter();
 
   async function verificaLogin(data: Inputs) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_URL_API}/clientes/login`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({ email: data.email, senha: data.senha }),
-      }
-    );
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/clientes/login`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ email: data.email, senha: data.senha }),
+    });
     if (response.status === 200) {
       const dados = await response.json();
       logaCliente(dados);
-      
-      Cookies.set("admin_logado_id", dados.id)
-      Cookies.set("admin_logado_nome", dados.nome)
+
+      Cookies.set("admin_logado_id", dados.id);
+      Cookies.set("admin_logado_nome", dados.nome);
       // Cookies.set("admin_logado_token", dados.token)
 
-      if (data.continuar) {
-        localStorage.setItem("client_key", dados.id);
-      } else {
-        if (localStorage.getItem("client_key")) {
-          localStorage.removeItem("client_key");
-        }
-      }
-
-      router.push("/principal") 
+      router.push("/principal");
     } else {
       toast({
         variant: "destructive",
@@ -62,68 +50,21 @@ export default function Login() {
       <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-16 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Login no Sistema
-            </h1>
-            <form
-              className="space-y-4 md:space-y-6"
-              onSubmit={handleSubmit(verificaLogin)}
-            >
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Login no Sistema</h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(verificaLogin)}>
               <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Seu email
                 </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                  {...register("email")}
-                />
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required {...register("email")} />
               </div>
               <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Senha
                 </label>
-                <input
-                  type="password"
-                  id="password"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
-                  {...register("senha")}
-                />
+                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required {...register("senha")} />
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      {...register("continuar")}
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      htmlFor="remember"
-                      className="text-gray-500 dark:text-gray-300"
-                    >
-                      Continuar Logado?
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-yellow-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
+              <button type="submit" className="w-full text-white bg-yellow-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Entrar
               </button>
             </form>
