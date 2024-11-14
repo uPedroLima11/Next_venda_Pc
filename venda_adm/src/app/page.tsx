a97d6f651d3b8acfbeb94a56c43026bce1455313
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-11-14 20:04:47 -0300

    Show Password

--- venda_adm/src/app/page.tsx
+++ venda_adm/src/app/page.tsx
@@ -6,6 +6,9 @@ import { ToastAction } from "@/components/ui/toast";
 import { useClienteStore } from "@/context/cliente";
 
 import Cookies from "js-cookie";
+import { useState } from "react";
+import { Eye } from "lucide-react";
+import { EyeClosedIcon } from "@radix-ui/react-icons";
 
 type Inputs = {
   email: string;
@@ -17,6 +20,7 @@ export default function Login() {
   const { toast } = useToast();
   const { logaCliente } = useClienteStore();
   const router = useRouter();
+  const [visivel, setVisivel] = useState(false);
 
   async function verificaLogin(data: Inputs) {
     const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/clientes/login`, {
@@ -59,10 +63,15 @@ export default function Login() {
                 <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required {...register("email")} />
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <label htmlFor={"password"} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Senha
                 </label>
-                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required {...register("senha")} />
+                <div className="relative mb-6">
+                  <input type={visivel ? "text" : "password"} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required {...register("senha")} />
+                  <div className="absolute cursor-pointer inset-y-0 end-0 flex items-center pe-3.5" onClick={() => setVisivel(!visivel)}>
+                    {visivel ? <Eye className="text-gray-400" /> : <EyeClosedIcon className="text-gray-400" />}
+                  </div>
+                </div>
               </div>
               <button type="submit" className="w-full text-white bg-yellow-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Entrar
