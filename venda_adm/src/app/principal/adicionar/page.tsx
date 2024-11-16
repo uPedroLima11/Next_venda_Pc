2ae4e3a5db737a256e75066ffe6e4430578eb35a
uPedroLima11 <pedromendeslima2016@gmail.com>
2024-11-16 17:54:30 -0300

    FiltrarProdutosListados

--- venda_adm/src/app/principal/adicionar/page.tsx
+++ venda_adm/src/app/principal/adicionar/page.tsx
@@ -75,7 +75,6 @@ export default function Adicionar() {
 
   return (
     <section className="mb-10">
-      <br />
       <div className="flex flex-col items-center px-6 py-8 mx-auto lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-16 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 m space-y-4 md:space-y-6 sm:p-8">
