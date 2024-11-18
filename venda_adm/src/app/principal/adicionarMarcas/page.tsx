34042557428b7e325361d743a10a264dc0a84ced
uPedroLima11 <pedromendeslima2016@gmail.com>
2024-11-18 19:34:31 -0300

    addMarcas Adicionado

---
+++ venda_adm/src/app/principal/adicionarMarcas/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { useToast } from "@/hooks/use-toast";
+
+type Inputs = {
+    nome: string;
+};
+
+export default function AdicionarMarca() {
+    const { register, handleSubmit } = useForm<Inputs>();
+    const { toast } = useToast();
+
+    async function adicionarMarca(data: Inputs) {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/marcas`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ nome: data.nome }),
+        });
+
+        if (response.status === 201) {
+            const dados = await response.json();
+            console.log(dados);
+            toast({
+                variant: "default",
+                title: "Marca criada com sucesso",
+                description: `A marca "${dados.nome}" foi adicionada ao sistema.`,
+            });
+        } else {
+            toast({
+                variant: "destructive",
+                title: "Erro ao criar marca",
+                description: "Verifique se os dados estão corretos.",
+            });
+        }
+    }
+
+    return (
+        <section className="mb-10">
+            <div className="flex flex-col items-center px-6 py-8 mx-auto lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-16 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 m space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Adicionar Marca
+                        </h1>
+                        <form
+                            className="space-y-4 md:space-y-6"
+                            onSubmit={handleSubmit(adicionarMarca)}
+                        >
+                            <div>
+                                <label
+                                    htmlFor="nome"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Nome da Marca
+                                </label>
+                                <input
+                                    type="text"
+                                    id="nome"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required
+                                    {...register("nome")}
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-white  bg-yellow-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            >
+                                Enviar
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ venda_adm/src/app/principal/listarMarcas/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface Marca {
+  id: number;
+  nome: string;
+}
+
+export default function MarcasList() {
+  const [marcas, setMarcas] = useState<Marca[]>([]);
+
+  useEffect(() => {
+    async function fetchMarcas() {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/marcas`);
+        const data = await response.json();
+        setMarcas(data);
+      } catch (error) {
+        console.error("Erro ao carregar marcas:", error);
+      }
+    }
+    fetchMarcas();
+  }, []);
+
+  return (
+    <section className="max-w-7xl mx-auto my-10">
+      <div className="flex justify-between items-center mb-6 mt-4">
+        <h1 className="text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
+          Lista de <span className="underline underline-offset-3 decoration-8 decoration-orange-400 dark:decoration-orange-600">Marcas</span>
+        </h1>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {marcas.map((marca) => (
+          <div
+            key={marca.id}
+            className="flex flex-col items-center bg-white rounded-lg shadow-md dark:bg-gray-800 p-4"
+          >
+            <div className="text-center">
+              <h2 className="text-lg font-bold text-gray-800 dark:text-white">
+                {marca.nome}
+              </h2>
+              <p className="text-sm text-gray-500 dark:text-gray-400">
+                <b>ID:</b> {marca.id}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {marcas.length === 0 && (
+        <div className="text-center text-gray-500 dark:text-gray-400 mt-10">
+          <p>Nenhuma marca encontrada.</p>
+        </div>
+      )}
+    </section>
+  );
+}
